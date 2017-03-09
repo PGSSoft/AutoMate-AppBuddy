7eb163c4e6e929a166864ad18100cb53a2c7e68f
Bartosz Janda <bjanda@pgs-soft.com>
2017-03-09 11:10:41 +0100

    PAI-109 Added LaunchEnvironmentResource.

--- AutoMate-AppBuddy/LaunchEnviromentResource.swift
+++ AutoMate-AppBuddy/LaunchEnviromentResource.swift
@@ -9,14 +9,45 @@
 import Foundation
 
 // MARK: - Launch Environment Resource
+
+/// Stores "resource" as a name of the resource and `Bundle` in which the resource is available.
+///
+/// Resource is initialized with a bundle description (a bundle name or a bundle identifier) and a name of the resource.
+/// If bundle name is `nil` then `Bundle.main` is used.
+///
+/// Resource can be parsed from string.
+///
+/// Resources representation: is made of "resource representation" separated by comma.
+/// In addition, the `CleanFlag` can be used as first element.
+///
+/// Resource representation: is made of a bundle name (or a bundle identifier) and a resource name
+/// separated with colon. Bundle name can be a `nil` string then the `Bundle.main` will be used.
+///
+/// **Example:**
+///
+/// ```
+/// Test data:events,nil:contacts
+/// AM_CLEAN_DATA_FLAG,Test data:events,nil:contacts
+/// ```
 public struct LaunchEnvironmentResource {
 
     // MARK: Properties
+    /// Clean flag indicating that existing data should be removed (eg. contacts) before adding new one.
     public static let CleanFlag = "AM_CLEAN_DATA_FLAG"
+
+    /// `Bundle` object where resource is available.
     public let bundle: Bundle
+
+    /// Resource name.
     public let name: String
 
     // MARK: Initialization
+    /// Initialize the resource with a bundle description (a bundle name or a bundle identifier) and a name of the resource.
+    ///
+    /// - Parameters:
+    ///   - bundleDescription: Bundle name or bundle identifier. If `nil` `Bundle.main` will be used.
+    ///   - name: Name of the resource.
+    /// - Returns: `nil` if cannot find a bundle with given name or identifier.
     public init?(bundle bundleDescription: String?, name: String) {
         guard let bundle = Bundle.with(stringDescription: bundleDescription) else { return nil }
         self.bundle = bundle
@@ -24,6 +55,20 @@ public struct LaunchEnvironmentResource {
     }
 
     // MARK: Static methods
+    /// Parse resource from string.
+    ///
+    /// String representation is made of a bundle name (or a bundle identifier) and a resource name
+    /// separated with colon. Bundle name can be a `nil` string then the `Bundle.main` will be used.
+    ///
+    /// **Example:**
+    ///
+    /// ```
+    /// Test data:events
+    /// nil:contacts
+    /// ```
+    ///
+    /// - Parameter resourceString: String representation of the resource.
+    /// - Returns: `nil` if cannot find a bundle with given name or identifier.
     public static func resource(from resourceString: String) -> LaunchEnvironmentResource? {
         let pair = resourceString.components(separatedBy: ":")
         let bundleName = pair.first != nil && pair.first != "nil" ? pair.first : nil
@@ -35,6 +80,23 @@ public struct LaunchEnvironmentResource {
         return resource
     }
 
+    /// Parse resources from string.
+    ///
+    /// String representation is made of "resource representation" separated by comma.
+    /// In addition, the `CleanFlag` can be used as first element.
+    ///
+    /// Resource representation: is made of a bundle name (or a bundle identifier) and a resource name
+    /// separated with colon. Bundle name can be a `nil` string then the `Bundle.main` will be used.
+    ///
+    /// **Example:**
+    ///
+    /// ```
+    /// Test data:events,nil:contacts
+    /// AM_CLEAN_DATA_FLAG,Test data:events,nil:contacts
+    /// ```
+    ///
+    /// - Parameter resourcesString: String representation of the resources.
+    /// - Returns: List of resources and `clean` flag.
     public static func resources(from resourcesString: String) -> ([LaunchEnvironmentResource], Bool) {
         var pairs = resourcesString.components(separatedBy: ",")
         var isCleanFlag = false
--- AutoMate-AppBuddy/LaunchOptionsKeys.swift
+++ AutoMate-AppBuddy/LaunchOptionsKeys.swift
@@ -9,9 +9,24 @@
 import Foundation
 
 // MARK: - Auto Mate Launch Option Key
+
+/// Represents key used in the launch environments handled by AppBudy.
+///
+/// - `animation`: Used to enable / disable animation, used by the `AnimationHandler`.
+/// - `contacts`: Used to manage contacs by the `ContactsHandler`.
+/// - `events`: Used to manage events by the `EventKitHandler`.
+/// - `reminders`: Used to manage reminders by the `EventKitHandler`.
 public enum AutoMateLaunchOptionKey: LaunchOptionKey {
+
+    /// Used to enable / disable animation, used by the `AnimationHandler`.
     case animation = "AM_ANIMATION_KEY"
+
+    /// Used to manage contacs by the `ContactsHandler`.
     case contacts = "AM_CONTACTS_KEY"
+
+    /// Used to manage events by the `EventKitHandler`.
     case events = "AM_EVENTS_KEY"
+
+    /// Used to manage reminders by the `EventKitHandler`.
     case reminders = "AM_REMINDERS_KEY"
 }
