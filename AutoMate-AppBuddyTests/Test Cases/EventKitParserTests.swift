a8c7d32ef163477ca7fae72d02cbf8ce8fe6d0e2
Joanna Bednarz <jbednarz@pgs-soft.com>
2017-11-06 13:36:00 +0100

    PAI-132 Fixed EKRecurrenceRule array assertion.

--- AutoMate-AppBuddyTests/Test Cases/EventKitParserTests.swift
+++ AutoMate-AppBuddyTests/Test Cases/EventKitParserTests.swift
@@ -115,4 +115,15 @@ class EventKitParserTests: XCTestCase {
             XCTFail("Types \(argument.debugDescription) and \(expected.debugDescription) do not match.", file: file, line: line)
         }
     }
+
+    func assert(countOf argument: [EKRecurrenceRule]?, isEqual expected: Any?, file: StaticString = #file, line: UInt = #line) {
+        switch expected {
+        case .none:
+            XCTAssertNil(argument, "Argument is \(argument.debugDescription) while expected is .none", file: file, line: line)
+        case let aCollection as [Any]:
+            XCTAssertEqual(aCollection.count, argument?.count, "Value count \(argument?.count ?? 0) is not equal to expected \(aCollection.count).", file: file, line: line)
+        case .some:
+            XCTFail("Types \(argument.debugDescription) and \(expected.debugDescription) do not match.", file: file, line: line)
+        }
+    }
 }
